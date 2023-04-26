4cdfd275f25a467d1c24edc346b5bb53338d5010
fallra1n <tozhimukhammedov.aa@yandex.ru>
2023-04-27 00:42:34 +0300

    added dockerfile and docker compose for redis

--- secretmessenger/secretmessenger/settings.py
+++ secretmessenger/secretmessenger/settings.py
@@ -74,10 +74,10 @@ WSGI_APPLICATION = 'secretmessenger.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
     }
 }
 
