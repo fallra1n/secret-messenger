2d02d1931d5f4ef09aeb4b612145b92f5387ec45
fallra1n <tozhimukhammedov.aa@yandex.ru>
2023-04-27 02:21:00 +0300

    added html form for post request

--- secretmessenger/smessages/views.py
+++ secretmessenger/smessages/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 
-def index(request):
-    return render(request, '')
+def index(request) -> HttpResponse:
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        ttl = request.POST.get('ttl')
+
+        return render(request, 'smessages/key.html', {'key': message + ttl})
+
+    return render(request, 'smessages/index.html')
 
 
-def get_secret_message(request, key: str):
+def get_secret_message(request, key: str) -> HttpResponse:
     return HttpResponse(f"{key}")
 
 
-def page_not_found(request, exception):
-    return HttpResponseNotFound("str not found")
+def page_not_found(request, exception) -> HttpResponseNotFound:
+    return render(request, 'smessages/404.html')
