5a4c84f3112cf467f03ab5c546f1456dd5ade08f
fallra1n <tozhimukhammedov.aa@yandex.ru>
2023-04-27 13:23:10 +0300

    added local storage for messages

--- secretmessenger/smessages/views.py
+++ secretmessenger/smessages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from uuid import uuid4
+
+storage = {}
 
 
 def index(request) -> HttpResponse:
@@ -7,13 +10,19 @@ def index(request) -> HttpResponse:
         message = request.POST.get('message')
         ttl = request.POST.get('ttl')
 
-        return render(request, 'smessages/key.html', {'key': message + ttl})
+        uid = uuid4()
+        global storage
+        storage[str(uid)] = message
+        host = request.META.get('HTTP_HOST')
+
+        return render(request, 'smessages/key.html', {'key': 'http://' + str(host) + '/smessenger/smessages/' + str(uid) + '/'})
 
     return render(request, 'smessages/index.html')
 
 
 def get_secret_message(request, key: str) -> HttpResponse:
-    return HttpResponse(f"{key}")
+    message = storage[key]
+    return render(request, 'smessages/message.html', {'message': message})
 
 
 def page_not_found(request, exception) -> HttpResponseNotFound:
